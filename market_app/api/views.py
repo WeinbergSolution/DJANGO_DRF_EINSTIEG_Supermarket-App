ed920fb8763f3027ea5df6641af25a1dda666ffa
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-24 15:39:59 +0200

    Add POST handling with try-except to first API view

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -1,6 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view() # dekorater
+@api_view(['GET', 'POST']) # dekorater
 def first_view(request):
-    return Response({"message": "Hello, world!"})
+    if request.method == 'GET':
+        return Response({"message": "Hello, World!"})
+    if request.method == 'POST':
+            try:
+                 msg = request.data['message']
+                 return Response({"your_message": msg})
+            except:
+                 return Response({"message": "error"})
