aa1c035b7e0b81e0d8f8af7aa78a0e274007a654
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-24 08:38:18 +0200

    Add market models and database migrations

--- supermarket/settings.py
+++ supermarket/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'market_app'
+    'rest_framework',
+    'market_app',
 ]
 
 MIDDLEWARE = [
